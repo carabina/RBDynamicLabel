3eebf1bb32941e04b4bac720b9c202ac5f25520c
Rich Blanchard <blanchard5595@yahoo.com>
2017-03-02 16:22:27 -0700

    initial commit

---
+++ RBDynamicLabel.swift
@@ -0,0 +1,54 @@
+//
+//  DynamicUILabel.swift
+//
+//  Created by Rich Blanchard on 3/2/17.
+//  Copyright © 2017 Rich. All rights reserved.
+//
+
+import UIKit
+
+
+class RBDynamicLabel: UILabel {
+    
+    private lazy var heightOfLabel:NSLayoutConstraint = {
+        return NSLayoutConstraint(item: self, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: self.bounds.height)
+    }()
+    private lazy var MAX_SIZE:CGSize = {
+        return CGSize(width: self.bounds.size.width, height: CGFloat.greatestFiniteMagnitude)
+    }()
+    override var text: String? {
+        didSet {
+            getNewSizeOfLabel()
+        }
+    }
+    private func getNewSizeOfLabel() {
+        heightOfLabel.constant = getNewRect(fromMaxSize: MAX_SIZE).height
+        
+    }
+    private func getNewRect(fromMaxSize maxSize: CGSize) -> CGSize {
+        let attributedString = NSAttributedString(string: text!, attributes: [NSFontAttributeName:self.font])
+        let newRect = attributedString.boundingRect(with: maxSize, options: NSStringDrawingOptions.usesLineFragmentOrigin, context: nil)
+        let newSize = CGSize(width: newRect.size.width, height: newRect.size.height)
+        return newSize
+    }
+    
+    private func addConstraints() {
+        self.addConstraints([heightOfLabel])
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        addConstraints()
+    }
+    
+    
+    
+    
+}
